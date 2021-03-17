cf2c8ee1975dffffe3a981cbdc94764841fdda3c
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-03-17 16:28:39 +0330

    create secret_key file

--- mhq_web/settings.py
+++ mhq_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secret_code import SECRET_KEY_GMAIL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'talkzone2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'tnniavqaqermotzx'
+EMAIL_HOST_PASSWORD = SECRET_KEY_GMAIL
 EMAIL_USE_TLS = True
 
 # Internationalization
