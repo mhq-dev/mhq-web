fff0368b1621f824ba52e77ad6708260549767ae
AmirH Ahmadi <amirh.khali@gmail.com>
2021-04-10 17:34:14 +0430

    change api url for get profile

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from authentication.viewsets import *
 
 urlpatterns = [
-    path('profile/<str:username>/', get_user_profile),
+    path('profile/user/<str:username>/', get_user_profile),
     path('profile/update/', update_user_profile)
 ]
