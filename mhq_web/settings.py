a00da518be759488024f64f1fb1f5fe4864386c7
AmirH Ahmadi <amirh.khali@gmail.com>
2021-04-10 18:33:33 +0430

    add get image url

--- mhq_web/settings.py
+++ mhq_web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -167,3 +167,5 @@ AUTH_USER_MODEL = 'authentication.User'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
