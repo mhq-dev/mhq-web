9e1c408c4ab4a0207d151f75c2f2597e909e0101
AmirH Ahmadi <amirh.khali@gmail.com>
2021-04-12 15:03:55 +0430

    nothing

--- request/views.py
+++ request/views.py
@@ -23,10 +23,3 @@ class RequestViewSet(viewsets.ModelViewSet):
         collection = get_object_or_404(Collection, id=collection_id)
         requests = Request.objects.all().filter(collection=collection)
         return JsonResponse(RequestFullSerializer(requests, many=True).data, safe=False)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        # self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
