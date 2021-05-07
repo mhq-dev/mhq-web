85f7433648c7a4df8dee2cde2bba4ba3b5e0bfa8
AmirH Ahmadi <amirh.khali@gmail.com>
2021-05-07 07:09:43 +0430

    add all request api test

--- request/permissions.py
+++ request/permissions.py
@@ -1,17 +1,30 @@
 from rest_framework import permissions
 
-from collection.models import UserCollection, Collection
+from collection.models import UserCollection
 
 
 class RequestPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method == 'GET':
+        if view.action == 'update' or view.action == 'destroy':
             user_collection = UserCollection.objects.filter(user=request.user, collection=obj.collection)
-            if obj.collection.type == Collection.PRIVATE and len(user_collection) != 1:
+            if len(user_collection) != 1 or user_collection[0].role == UserCollection.VISITOR:
                 return False
-        else:
-            user_collection = UserCollection.objects.filter(user=request.user, collection=obj.collection)
+
+        return True
+
+    def has_permission(self, request, view):
+        if view.action == 'update' or view.action == 'destroy' or view.action == 'create':
+            if not request.user.is_authenticated:
+                return False
+
+        if view.action == 'create':
+            # TODO find better idea for this
+            if 'collection' not in request.data:
+                return False
+
+            user_collection = UserCollection.objects.filter(user=request.user,
+                                                            collection__id=request.data['collection'])
             if len(user_collection) != 1 or user_collection[0].role == UserCollection.VISITOR:
                 return False
 
