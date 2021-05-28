76b0e47d85879e1d0b134df5e627be45516bb965
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-05-28 23:25:46 +0430

    basic api worked fine

--- condition/urls.py
+++ condition/urls.py
@@ -4,4 +4,6 @@ from . import viewsets
 urlpatterns = [
     path('available_condition/', viewsets.available_cond),
     path('<int:pk>', viewsets.detail_cond),
+    path('', viewsets.create_cond),
+
 ]
--- condition/viewsets.py
+++ condition/viewsets.py
@@ -8,3 +8,7 @@ detail_cond = ConditionViewSet.as_view({
     'put': 'update',
     'delete': 'destroy',
 })
+
+create_cond = ConditionViewSet.as_view({
+    'post': 'create'
+})
