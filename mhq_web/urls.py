761806b2b9b3ccba1e128aa4b594a828fb6e10d8
AmirH Ahmadi <amirh.khali@gmail.com>
2021-06-01 02:05:02 +0430

    change django static location

--- mhq_web/urls.py
+++ mhq_web/urls.py
@@ -18,9 +18,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^api/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^api/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('api/admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
